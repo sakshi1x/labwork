26794fe2873a4b8bc06fa9c8c5531c2b2343ea2b
sakshi1x <nepalsakshi05@gmail.com>
2021-12-01 12:49:27 +0545

    third cmmit

--- labwork2.py
+++ labwork2.py
@@ -1,4 +1,4 @@
 l = int(input("enter a height of a triangle: "))
 b = int(input("enter a breath of a tiangle: "))
 a = (l*b)/2
-print("area of a triangle is : ", a)
+print("area of a triangle is {}cm\u00b2".format(a))
