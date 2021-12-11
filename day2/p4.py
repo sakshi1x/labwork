778271dd022689b5f80900ffec0e079df4f1e231
sakshi1x <nepalsakshi05@gmail.com>
2021-12-11 10:25:00 +0545

    initial commit

---
+++ day2/p4.py
@@ -0,0 +1,6 @@
+n = int(input("enter the number of apples available: "))
+stu = int(input("enter the number of students : "))
+c = n // stu
+d = n% stu
+print(f"each students get {c} apples.")
+print(f"{d} appels remains in the basket.")
