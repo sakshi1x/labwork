dd88b9e66fcc5631d176e036e76e7dfc63cd97f3
sakshi1x <nepalsakshi05@gmail.com>
2021-12-11 10:45:49 +0545

    initial commit

---
+++ day2/p5.py
@@ -0,0 +1,15 @@
+a = int(input("enter the numbers of students in class a : "))
+b = int(input("enter the numbers of students in class b: "))
+c = int(input("enter the numbers of students in class c:"))
+a1 = a//2
+a2 = a%2
+b1 = b //2
+b2 = b%2
+c1 = c//2
+c2 = c%2
+a3 = a1 + a2
+b3 = b1 +b2
+c3 = c1 + c2
+print(a3 , "desks in class a .")
+print(b3 , " desks in class b")
+print( c3 , "desks needed in class c.")
