61da74eecc478ab580b8307ab19cf88b673c1b8d
sakshi1x <nepalsakshi05@gmail.com>
2021-12-11 11:06:09 +0545

    initial commit

---
+++ day2/p6.py
@@ -0,0 +1,12 @@
+s = 25 
+d = 4
+t = s/d
+t1 = t + ((2*10)/60)
+t2 = 7/1
+t3 = 15/2
+t4= 7/1
+t5 = t1 +t2+t3+t4
+if t1>t5:
+    print("bus will be quicker.")
+else:
+    print("bus will be slower.")
