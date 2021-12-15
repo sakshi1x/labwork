7e2fe901d15efe7b3a07c8bca64bb79804d3b5cb
sakshi1x <nepalsakshi05@gmail.com>
2021-12-15 18:04:06 +0545

    initial commit

---
+++ day5/day9.py
@@ -0,0 +1,11 @@
+num = int(input("enter a number: "))
+factorial = 1
+if num <0:
+    print("doesnot exist for negative numbers.")
+elif num == 0:
+    print(f"{factorial}  if factorial of 0.")
+else:
+    for i in range(1,num+1):
+        factorial = i * factorial
+    print(f"{factorial} is facorial of{num} .")
+    
