263ee2d4eb60ee6e1b104bb064f1931c0916c063
sakshi1x <nepalsakshi05@gmail.com>
2021-12-15 13:29:25 +0545

    initial commit

---
+++ day5/day4.py
@@ -0,0 +1,13 @@
+k= 5
+for i in range(5):
+    for j in range(i):
+        print("*", end= '')
+    print("\r")
+while k >=0:
+    for a in range(k):
+        print("*", end ='')
+    print("\r")
+    k-= 1
+    
+    
+        
