bde6fc4916f9d1a956c9380aaec357f7c1fba569
sakshi1x <nepalsakshi05@gmail.com>
2021-12-15 12:18:02 +0545

    initial commit

--- day 3/lab1.py
+++ day 3/lab1.py
@@ -1,12 +1,3 @@
-a = 0
-l = [1,2,3,4,5]
-for x in l:
-    if x ==5:
-        print("5 is in list")
-        a+=1
-        
-        break
-if a == 0:
-    print("5 is not in list")
+ 
         
     
---
+++ day5/day1.py
@@ -0,0 +1,4 @@
+for x in range(1500,2700):
+    if x%7 == 0 and x%5 ==0:
+        print(x)
+        
