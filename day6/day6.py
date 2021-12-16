4e607890a2a5a95c00045c482357083c7c7308cc
sakshi1x <nepalsakshi05@gmail.com>
2021-12-16 09:07:40 +0545

    initial commit

---
+++ day6/day6.py
@@ -0,0 +1,5 @@
+x = [1,2]
+if len(x) > 0 :
+    print("not empty")
+else:
+    print("empty")
---
+++ day6/day9.py
@@ -0,0 +1,5 @@
+import random
+x= [1,2,3,4]
+print("given list is ", x)
+y = random.randrange(len(x))
+print("andom number is : ", x[y])
