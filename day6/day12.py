899867f843c0bc533380584d55ea058314e0ffe1
sakshi1x <nepalsakshi05@gmail.com>
2021-12-16 08:49:34 +0545

    initial commit

---
+++ day6/day12.py
@@ -0,0 +1,4 @@
+x = [1,2,3,4]
+for y in x:
+    y *=y
+print(y)
---
+++ day6/day5.py
@@ -0,0 +1,10 @@
+ctr = 0
+x = ["aba","abc","1221","abt"]
+for a in x:
+    if a[0]==a[-1]:
+        pass
+    else:
+        ctr += 1
+print(ctr)
+
+        
